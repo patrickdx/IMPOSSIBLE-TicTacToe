64f7f828a0a8c3ec946f552a7743f8903b500f39
patrickdx <blah0833@gmail.com>
2023-11-16 11:04:58 -0500

    Add files via upload

---
+++ minimax.py
@@ -0,0 +1,144 @@
+import copy
+
+class Board:
+    text  =  '\n   {}  |  {}  |  {}  \n _____|_____|_____\n      |     |     \n   {}  |  {}  |  {}  \n _____|_____|_____\n      |     |     \n   {}  |  {}  |  {}  \n      |     |     \n\n'
+
+    
+    def __init__(self):
+        self.board = [
+            ['-', '-', '-'],
+            ['-', '-', '-'],
+            ['-', '-', '-'],     
+        ]
+        self.turn = 'X'    
+
+    
+
+    def __repr__(self):
+        board = self.board  
+        return (Board.text.format(*(board[0] + board[1] + board[2])))             # unpacks elements and passes it as arguments
+
+    def move(self, x,y):
+        if x >= 0 and x < len(self.board) and y >=0 and y < len(self.board) and self.board[x][y] == '-':
+            self.board[x][y] = self.turn
+            self.turn = 'O' if self.turn == 'X' else 'X'
+            
+        else: raise ValueError("Invalid move")
+           
+
+    def isWon(self):
+        def checkRows(board):
+            for row in board:
+                if len(set(row)) == 1 and row[0] != '-': return True
+            return False
+    
+        def checkCols(board):
+            return checkRows(list(zip(*board)))     #transpose cols -> rows
+
+        def checkDiags(board):
+            diag = []
+            diag1 = []
+    
+            for i in range(len(board)):
+                diag.append(board[i][i])
+                diag1.append(board[i][2-i])
+                
+            return ('-' not in diag and len(set(diag)) == 1) or ('-' not in diag1 and len(set(diag1)) == 1)
+            
+        for check in [checkRows, checkCols, checkDiags]:
+            if check(self.board): return True
+            
+        return False
+
+    def legal_moves(self):       # Possible moves for the current turn
+        moves = []
+        
+        for i in range(len(self.board)):
+            for j in range(len(self.board)):
+                if self.board[i][j] == '-': moves.append((i,j))
+        return moves
+               
+            
+
+def evaluation(board):         # eval function to eval board state, only doable if won or draw
+    if board.isWon() and board.turn == 'O': return 10
+    elif board.isWon() and board.turn == 'X': return -10
+    elif len(board.legal_moves()) == 0: return 0        # draw 
+    
+
+best_move = ()
+def minimax(board : Board):      # returns the best score possible from state given opponent plays optimally
+    global best_move
+    move_scores = {} 
+    
+    
+    # base case: once reached end_state (win/draw), call evaluation return score
+    if evaluation(board) != None: return evaluation(board)
+
+    for x,y in board.legal_moves():
+       new_state = copy.deepcopy(board)           # create new board state, recursive call
+       new_state.move(x,y)
+       move_scores[(x,y)] = minimax(new_state)    # the resulting score from going thru that state
+       
+    print(move_scores)
+    
+    if board.turn == 'X':         # select the max score
+        best_move = max(move_scores, key = move_scores.get)
+        return max(move_scores.values())
+
+    if board.turn == 'O':          # opponent wants to minimize our score as much as possible
+        best_move = min(move_scores, key = move_scores.get)
+        return min(move_scores.values())
+    
+
+board = Board()
+
+#  board.move(0,2)
+# board.move(0,0)
+# board.move(1,0)
+# board.move(2,2)
+# board.move(2,0)
+# board.move(2,1)
+
+def promptMove(prompt):
+    while True:
+        try:
+            move = input(prompt)
+            x,y = map(int, move.split())
+            board.move(x,y)
+            break
+        except ValueError: 
+            print("Invalid move")
+
+def main():
+                
+    # Start playing vs computer
+    while len(board.legal_moves()) != 0: 
+
+        promptMove('Player 1 Move:')
+        print(board)
+        
+        minimax(board)
+        print("Computer Turn: ", best_move)
+        board.move(best_move[0], best_move[1])  
+        
+        print(board)
+        if evaluation(board) != None: 
+            print("Game over!", board)
+            break
+             
+        
+
+
+
+
+if __name__ == '__main__': # test if either run-directly or being imported
+    main()    
+
+
+
+
+
+  
+
+
